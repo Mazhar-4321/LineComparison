19d1d5ff52cb334ed876a1c1241368ecdeb66407
Mazhar-4321 <syedmazharali742@gmail.com>
2022-11-18 20:00:43 +0530

    ADDED: Code to find length of coordinates using Cartesian system

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,18 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Welcome to Line Comparison Computation Program");
+        Scanner scanner= new Scanner(System.in);
+        double x1=scanner.nextDouble();
+        double x2=scanner.nextDouble();
+        double y1=scanner.nextDouble();
+        double y2=scanner.nextDouble();
+        double length=Math.sqrt(Math.pow(x1-x2,2)+Math.pow(y1-y2,2));
+        System.out.println("length="+length);
     }
 }
