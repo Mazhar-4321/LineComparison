cedf4b422eec8f778f00153e93c5fd8a40821a09
Mazhar-4321 <syedmazharali742@gmail.com>
2022-11-18 20:33:39 +0530

    ADDED: Code to find one line is equal,greater or less than other line

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -15,10 +15,25 @@ public class Main {
         double length = Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
         System.out.println("length=" + length);
         String isEqual = "not equal";
+        // 2,6 2,7
         if (Double.compare(x1, y1) == 0 && Double.compare(x2, y2) == 0) {
             isEqual = "equal";
+        } else {
+            if (Double.compare(x1, y1) > 0) {
+                isEqual = "Line 1 is greater then Line 2";
+            } else {
+                if (Double.compare(x1, y1) < 0) {
+                    isEqual = "Line 2 is greater then Line 1";
+                } else {
+                    if (Double.compare(x2, y2) > 0) {
+                        isEqual = "Line 1 is greater then Line 2";
+                    } else {
+                        isEqual = "Line 2 is greater than Line 1";
+                    }
+                }
+            }
         }
-        System.out.println("Two Lines are " + isEqual);
+        System.out.println(isEqual);
 
     }
 }
