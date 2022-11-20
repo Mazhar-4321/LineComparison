3a082500a81181c7d63f98133bf4e9d761c6c5f2
Mazhar-4321 <syedmazharali742@gmail.com>
2022-11-20 19:07:34 +0530

    MODIFIED: Code to change the Primitive Type to Wrapper Type

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,14 +8,14 @@ public class Main {
         // write your code here
         System.out.println("Welcome to Line Comparison Computation Program");
         Scanner scanner = new Scanner(System.in);
-        double x1 = scanner.nextDouble();
-        double x2 = scanner.nextDouble();
-        double y1 = scanner.nextDouble();
-        double y2 = scanner.nextDouble();
-        double length = Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
+        Double x1 = scanner.nextDouble();
+        Double x2 = scanner.nextDouble();
+        Double y1 = scanner.nextDouble();
+        Double y2 = scanner.nextDouble();
+        Double length = Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
         System.out.println("length=" + length);
         String isEqual = "not equal";
-        if (Double.compare(x1, y1) == 0 && Double.compare(x2, y2) == 0) {
+        if (x1.equals(y1) && x2.equals(y2)) {
             isEqual = "equal";
         } else {
             if (Double.compare(x1, y1) > 0) {
