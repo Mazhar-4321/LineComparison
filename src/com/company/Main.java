9ef4d409e036a57793c699fc06a565366e244315
Mazhar-4321 <syedmazharali742@gmail.com>
2022-11-20 18:55:48 +0530

    MODIFIED: Code to replace Primitive Type with Wrapper Type

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,11 +8,11 @@ public class Main {
 	// write your code here
         System.out.println("Welcome to Line Comparison Computation Program");
         Scanner scanner= new Scanner(System.in);
-        double x1=scanner.nextDouble();
-        double x2=scanner.nextDouble();
-        double y1=scanner.nextDouble();
-        double y2=scanner.nextDouble();
-        double length=Math.sqrt(Math.pow(x1-x2,2)+Math.pow(y1-y2,2));
+        Double x1=scanner.nextDouble();
+        Double x2=scanner.nextDouble();
+        Double y1=scanner.nextDouble();
+        Double y2=scanner.nextDouble();
+        Double length=Math.sqrt(Math.pow(x1-x2,2)+Math.pow(y1-y2,2));
         System.out.println("length="+length);
     }
 }
