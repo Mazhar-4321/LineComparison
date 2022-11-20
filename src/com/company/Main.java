f9392188f5fcf26c53182807761c55371ec3f46d
Mazhar-4321 <syedmazharali742@gmail.com>
2022-11-20 19:16:22 +0530

    MODIFIED: Code to change the logic

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -14,7 +14,7 @@ public class Main {
         Double length = Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
         System.out.println("length=" + length);
         String isEqual = "not equal";
-        if (Double.compare(x1, y1) == 0 && Double.compare(x2, y2) == 0) {
+        if (x1.equals(y1) && x2.equals(y2)) {
             isEqual = "equal";
         }
         System.out.println(isEqual);
