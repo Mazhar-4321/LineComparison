4249a769544e5607f3880c283cedab848410efe4
Mazhar-4321 <syedmazharali742@gmail.com>
2022-11-20 19:05:10 +0530

    MODIFIED: Code to merge UC2_Branch

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        // write your code here
         System.out.println("Welcome to Line Comparison Computation Program");
         Scanner scanner = new Scanner(System.in);
         Double x1 = scanner.nextDouble();
@@ -14,23 +15,10 @@ public class Main {
         Double length = Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
         System.out.println("length=" + length);
         String isEqual = "not equal";
-        if (Double.compare(x1, y1) == 0 && Double.compare(x2, y2) == 0) {
+        if (x1.equals(y1) && x2.equals(y2)) {
             isEqual = "equal";
-        } else {
-            if (Double.compare(x1, y1) > 0) {
-                isEqual = "Line 1 is greater then Line 2";
-            } else {
-                if (Double.compare(x1, y1) < 0) {
-                    isEqual = "Line 2 is greater then Line 1";
-                } else {
-                    if (Double.compare(x2, y2) > 0) {
-                        isEqual = "Line 1 is greater then Line 2";
-                    } else {
-                        isEqual = "Line 2 is greater than Line 1";
-                    }
-                }
-            }
         }
-        System.out.println(isEqual);
+        System.out.println("Two Lines are " + isEqual);
+
     }
 }
